1ad2b1051f1403695628e2082c03e1f95ca4d6a8
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-12-03 11:35:40 +0000

    Add header

--- src/app/(main)/downloads/layout.tsx
+++ src/app/(main)/downloads/layout.tsx
@@ -1,20 +1,26 @@
+import Header from "@/components/downloads/Header";
 import { ReactNode } from "react";
 
 export default function DownloadsLayout({ children, sidebar }: { children: ReactNode, sidebar: ReactNode }) {
     return (
-        <div className="flex flex-col sm:flex-row-reverse min-h-screen bg-dark-850 text-light">
-            <div className="w-full sm:max-w-64 md:max-w-xs lg:max-w-md 2xl:max-w-2xl flex flex-col bg-dark-900 border-b sm:border-l sm:border-b-0 border-dark-700">
-                <div className="grow xl:pr-48">
-                    <section className="p-8">
-                        {sidebar}
-                    </section>
-                </div>
+        <div className="min-h-screen flex flex-col bg-dark-850 text-light">
+            <div className="border-b border-dark-700 bg-dark-850">
+                <Header />
             </div>
-            <div className="grow flex flex-col bg-dark-800">
-                <div className="grow xl:pl-48">
-                    <main className="p-8">
-                        {children}
-                    </main>
+            <div className="grow flex flex-col sm:flex-row-reverse">
+                <div className="w-full sm:max-w-64 md:max-w-xs lg:max-w-md 2xl:max-w-2xl flex flex-col bg-dark-900 border-b sm:border-l sm:border-b-0 border-dark-700">
+                    <div className="grow xl:pr-48">
+                        <section className="p-8">
+                            {sidebar}
+                        </section>
+                    </div>
+                </div>
+                <div className="grow flex flex-col bg-dark-800 overflow-y-scroll">
+                    <div className="grow xl:pl-48">
+                        <main className="p-8">
+                            {children}
+                        </main>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/downloads/Header.tsx
@@ -0,0 +1,53 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import headerIcon from "./header-icon.png"
+
+export default async function Header() {
+    const session = await auth()
+
+    return (
+        <div className="flex items-center ml-8 xl:mx-56">
+            <Link href={`/`} className="my-2">
+                <Image src={headerIcon} alt="tobymeehan.com"
+                    className="size-9 rounded-full" />
+            </Link>
+
+            <div className="text-xl ml-3 mr-1">
+                /
+            </div>
+
+            <Link href={`/downloads`} className="font-bold text-bright p-2 rounded-md transition hover:bg-dark-700 active:opacity-75">
+                Downloads
+            </Link>
+
+            {session
+                ? <>
+                    <Image src={`https://thavyra.xyz/api/users/${session.user?.id}/avatar.png`} alt="Avatar"
+                        width={500} height={500} className="size-6 rounded-full ml-auto" />
+                    <div className="text-bright ml-1.5">
+                        Hi {session.user?.name}!
+                    </div>
+                    <form className="ml-2" action={async () => {
+                        "use server"
+                        await signOut()
+                    }}>
+                        <button type="submit">
+                            Logout
+                        </button>
+                    </form>
+                </>
+                : <>
+                    <form action={async () => {
+                        "use server"
+                        await signIn("thavyra")
+                    }} className="ml-auto">
+                        <button type="submit">
+                            Log In
+                        </button>
+                    </form>
+                </>
+            }
+        </div>
+    )
+}
