33e4f3c0528ff9370b642a79a67f0d7f808ef7ed
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-12-10 22:30:18 +0000

    Update download list column spacing

--- src/components/downloads/DownloadList.tsx
+++ src/components/downloads/DownloadList.tsx
@@ -3,7 +3,7 @@ import DownloadLink from "./DownloadLink";
 
 export default function DownloadList({ downloads }: { downloads: Download[] }) {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8">
             {downloads.map(download => {
                 return (
                     <DownloadLink key={download.id} download={download} />
